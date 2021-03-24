eb1b851276ab04700967039b1d9e5e72105a5200
sowmyagavini <sowmyagavini0023@gmail.com>
2021-03-24 19:26:13 +0530

    Zombie attackkkk

---
+++ src/zombieKiller/ZombieKiller.java
@@ -0,0 +1,23 @@
+package zombieKiller;
+
+public class ZombieKiller {
+
+	public static void main(String[] args) {
+System.out.println("Welcome to Zombie Killer program!!");
+
+String backpack[]= { "shortgun", "Assault Rifle", "Sniper"	};
+String zombies[]= {"short_range","mid_range","long_range"};
+
+System.out.println("backpack items!!");
+System.out.println(backpack[0]);
+System.out.println(backpack[1]);
+System.out.println(backpack[2]);
+
+
+
+System.out.println("zombies!!");
+System.out.println(zombies[0]);
+System.out.println(zombies[1]);
+System.out.println(zombies[2]);
+	}
+}
